7bdbe0523d0c7d1f1db18ae855e52bad29438987
emiliodeleonhdez <emiliodeleon.contact@gmail.com>
2022-01-28 12:00:19 -0600

    Subiendo la Card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 // import BarcodeScannerComponent from "react-qr-barcode-scanner";
 import Button from '@mui/material/Button';
-import PrimaryButton from "./Components/Buttons/Primary/PrimaryButton";
+// import PrimaryButton from "./Components/Buttons/Primary/PrimaryButton";
+import MainCard from "./Components/Cards/MainCard";
 
 
 function App() {
@@ -9,8 +10,9 @@ function App() {
 
   return (
     <>
-    <PrimaryButton color="error">Login</PrimaryButton>
-    <PrimaryButton color="secondary">sign in</PrimaryButton>
+    {/* <PrimaryButton color="error">Login</PrimaryButton>
+    <PrimaryButton color="secondary">sign in</PrimaryButton> */}
+    <MainCard/>
     </>
   );
 }
