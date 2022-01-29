bb2dd82bc61d57ac0af2ee0bd4dfb013df49af37
NancySoriano <nancysoriano.m@gmail.com>
2022-01-28 21:28:19 -0600

    agregando Background

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from "react";
+import Background from './Components/Backgrounds/Background.jsx'
 // import BarcodeScannerComponent from "react-qr-barcode-scanner";
-import Button from '@mui/material/Button';
-import PrimaryButton from "./Components/Buttons/Primary/PrimaryButton";
-
 
 function App() {
   const [data, setData] = React.useState("Not Found");
-
   return (
     <>
-    <PrimaryButton color="error">Login</PrimaryButton>
-    <PrimaryButton color="secondary">sign in</PrimaryButton>
-    </>
+    <Background>
+      Hola
+    </Background>
+     </>
   );
 }
 
