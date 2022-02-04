21fec34d6480c57574cacd6a31fcd662e61166ca
Cesar Hernandez <devcesar.hdez@gmail.com>
2022-02-04 14:15:02 -0600

    Cambios en app.js

--- src/App.js
+++ src/App.js
@@ -8,11 +8,12 @@ import {
 } from "react-router-dom";
 import UserSettings from "./Pages/UserSettings/UserSettings";
 import Home from "./Pages/Home/Home";
-import Landing from "./Pages/Landing/Landing";
+import FeedingUser from "./Pages/FeedingUser/FeedingUser";
+
 function App() {
+  return (
     <BrowserRouter>
       <Routes>
-        <Route path="/landing" element={<Landing/>}/>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/usersettings" element={<UserSettings/>}/>
