5ccd86168a0225cab7b7c8113a4553555c0a25f3
Rodojimenez2236 <jimenezp2236@gmail.com>
2022-02-24 23:56:40 -0600

    changes to background

--- src/Components/Backgrounds/Background.jsx
+++ src/Components/Backgrounds/Background.jsx
@@ -4,20 +4,25 @@ import logochecalo from "../../Pages/Svg/logochecalo.svg";
 import { Grid, Typography } from "@mui/material";
 
 const Background = () => {
-  return (
-    <div className={styles.fondoVerde}>
-      <Grid container justifyContent="center" flexDirection="column" alignItems="center">
-        <Grid item marginTop={10}>
-          <img src={logochecalo} alt="logo checalo" height="100" width="100" />
-        </Grid>
-        <Grid item >
-        <Typography fontWeight="700" fontSize={25} color={"white"}>
-            checalo.mx
-        </Typography>
-        </Grid>
-      </Grid>
-    </div>
-  );
+    return (
+        <div className={styles.fondoVerde}>
+            <Grid container flexDirection="column" alignItems="center">
+                <Grid item>
+                    <img
+                        src={logochecalo}
+                        alt="logo checalo"
+                        height="100"
+                        width="100"
+                    />
+                </Grid>
+                <Grid item>
+                    <Typography fontWeight="700" fontSize={25} color={"white"}>
+                        checalo.mx
+                    </Typography>
+                </Grid>
+            </Grid>
+        </div>
+    );
 };
 
 export default Background;
